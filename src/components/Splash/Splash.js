ed957a5d7c90b6ad7cecb26b7425189ddc6185c6
Jamie Pittak <charrawr456@gmail.com>
2018-05-15 16:29:49 -0700

    add text and button to splash page

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -4,7 +4,9 @@ import classes from './Splash.css';
 const splash = (props) =>{
   return(
     <div className={classes.Splash}>
-      <h1>hi</h1>
+      <h1>Help bridge the gap</h1>
+      <h1 className={classes.subHeading}>Learn American Sign Language</h1>
+      <button>Learn More</button>
     </div>
   )
 }
